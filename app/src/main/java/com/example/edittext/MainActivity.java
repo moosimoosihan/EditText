8a1281d99812f60e84e69d13bda26bde0948ab9e
moosimoosihan <segun408@gmail.com>
2022-10-11 10:54:13 +0900

    Intent commit

--- app/src/main/java/com/example/edittext/MainActivity.java
+++ app/src/main/java/com/example/edittext/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.edittext;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,9 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_move;
+    private EditText et_test;
+    private String str;
     EditText et_id;
     Button btn_test;
 
@@ -27,7 +31,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        et_test = findViewById(R.id.et_test);
 
+
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                str = et_test.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra("str",str);
+                startActivity(intent); // 액티비티 이동.
+            }
+        });
 
 
 
