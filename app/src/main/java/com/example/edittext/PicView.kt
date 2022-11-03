ef87e032aecdf950bc1fe74d4089d5ac357bbd78
moosimoosihan <segun408@gmail.com>
2022-11-03 11:43:20 +0900

    list photo view

--- app/src/main/java/com/example/edittext/PicView.kt
+++ app/src/main/java/com/example/edittext/PicView.kt
@@ -1,34 +1,28 @@
 package com.example.edittext
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.edittext.databinding.ActivityPicViewBinding
 
 class PicView : AppCompatActivity() {
-    private var btn_home: Button? = null
 
+    private var btn_home: Button? = null
+    lateinit var galleryAdapter: GalleryAdapter
     lateinit var binding: ActivityPicViewBinding
+    var imageList: ArrayList<Uri> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPicViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-        //버튼 이벤트
-        binding.galleryBtn.setOnClickListener {
-
-            //갤러리 호출
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.type = "image/*"
-            activityResult.launch(intent)
-        }
 
         btn_home = findViewById(R.id.btn_home)
         btn_home?.setOnClickListener(View.OnClickListener {
@@ -36,6 +30,23 @@ class PicView : AppCompatActivity() {
             startActivity(intent) // 액티비티 이동.
         })
 
+        //adapter 초기화
+        galleryAdapter = GalleryAdapter(imageList, this)
+
+        //recyclerView 설정
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = galleryAdapter
+
+        //버튼 이벤트
+        binding.galleryBtn.setOnClickListener {
+
+            //갤러리 호출
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            //멀티 선택 기능
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            activityResult.launch(intent)
+        }
     }//onCreate
 
     //결과 가져오기
@@ -43,14 +54,27 @@ class PicView : AppCompatActivity() {
         ActivityResultContracts.StartActivityForResult()){
 
         //결과 코드 OK , 결가값 null 아니면
-        if(it.resultCode == RESULT_OK && it.data != null){
-            //값 담기
-            val uri  = it.data!!.data
+        if(it.resultCode == RESULT_OK){
 
-            //화면에 보여주기
-            Glide.with(this)
-                .load(uri) //이미지
-                .into(binding.imageView) //보여줄 위치
+            //멀티 선택은 clipData
+            if(it.data!!.clipData != null){ //멀티 이미지
+
+                //선택한 이미지 갯수
+                val count = it.data!!.clipData!!.itemCount
+
+                for(index in 0 until count){
+                    //이미지 담기
+                    val imageUri = it.data!!.clipData!!.getItemAt(index).uri
+                    //이미지 추가
+                    imageList.add(imageUri)
+                }
+            }else{ //싱글 이미지
+                val imageUri = it.data!!.data
+                imageList.add(imageUri!!)
+            }
+            galleryAdapter.notifyDataSetChanged()
         }
-    }
+
+    }//onCreate
+
 }
